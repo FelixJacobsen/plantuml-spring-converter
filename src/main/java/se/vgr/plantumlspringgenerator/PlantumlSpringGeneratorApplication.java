580a0fd608903c2f3a61faa3cb80bab03e26c818
Felix Jacobsen <felix.jacobsen@iths.se>
2023-02-27 16:28:01 +0100

    Initial commit without application.properties and pom.xml

---
+++ src/main/java/se/vgr/plantumlspringgenerator/PlantumlSpringGeneratorApplication.java
@@ -0,0 +1,13 @@
+package se.vgr.plantumlspringgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantumlSpringGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantumlSpringGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/vgr/plantumlspringgenerator/PlantumlSpringGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package se.vgr.plantumlspringgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantumlSpringGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
